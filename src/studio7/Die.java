6fe518a74a87230d9d654c56c25008db4ec60477
Mark Schulist <mschulist2@gmail.com>
2023-11-02 10:37:25 -0500

    die

---
+++ src/studio7/Die.java
@@ -0,0 +1,30 @@
+package studio7;
+
+public class Die {
+	
+	
+	public int n;
+	
+	public Die() {
+		n = 0;
+	}
+	
+	public Die(int nu) {
+		n = nu;
+	}
+	
+	public int random() {
+		int rand = (int) (Math.random() * (n)) + 1;
+		return rand;
+	}
+
+	public static void main(String[] args) {
+		Die die1 = new Die(6);
+		Die die2 = new Die(20);
+		
+		System.out.println(die1.random());
+		System.out.println(die2.random());
+
+	}
+
+}
